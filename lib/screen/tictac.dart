0274c1fe85508023bed61b1aa4d4a286842bd72c
Pratima <pratimaasopa23@gmail.com>
2023-12-09 19:06:56 +0530

    upadted new logic for Tic Tca feaure

--- lib/screen/tictac.dart
+++ lib/screen/tictac.dart
@@ -1,114 +1,58 @@
 import 'package:flutter/material.dart';
-class TicTacToeScreen extends StatefulWidget {
+
+class Game extends StatefulWidget {
+  const Game({super.key});
+
   @override
-  _TicTacToeScreenState createState() => _TicTacToeScreenState();
+  State<Game> createState() => _GameState();
 }
 
-class _TicTacToeScreenState extends State<TicTacToeScreen> {
-  List<List<String>> board = List.generate(3, (i) => List.filled(3, ""));
-
-  bool isPlayerOneTurn = true; // true for player one, false for player two
-  String winner = "";
-
+class _GameState extends State<Game> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tic Tac Toe'),
-      ),
-      body: SingleChildScrollView(
+      backgroundColor: Colors.orange,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              winner.isNotEmpty ? 'Winner: $winner' : isPlayerOneTurn ? 'Player One' : 'Player Two',
-              style: TextStyle(fontSize: 20.0),
+            Expanded(
+              flex: 1,
+              child: Text('Score Board'),
             ),
-            SizedBox(height: 20.0),
-            GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 2.0,
-                mainAxisSpacing: 2.0,
-              ),
-              itemCount: 9,
-              itemBuilder: (context, index) {
-                int row = index ~/ 3;
-                int col = index % 3;
-                return GestureDetector(
-                  onTap: () => _onTileClicked(row, col),
-                  child: Container(
-                    height: 20,
-                    width: 20,
-                    color: Colors.blue,
-                    child: Center(
-                      child: Text(
-                        board[row][col],
-                        style: TextStyle(fontSize: 30.0, color: Colors.white),
+            Expanded(
+              flex: 3,
+              child: GridView.builder(
+                itemCount: 9,
+                gridDelegate:
+                    SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,),
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(),
+                        color: Colors.blueAccent,
+                      ),
+                      child: Center(
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('images/speak.jpeg'),
+                          radius: 30,
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _resetGame,
-              child: Text('Reset Game'),
+            Expanded(
+              flex: 2,
+              child: Text('Timer'),
             ),
           ],
         ),
       ),
     );
-  }
-
-  void _onTileClicked(int row, int col) {
-    if (board[row][col].isEmpty && winner.isEmpty) {
-      setState(() {
-        board[row][col] = isPlayerOneTurn ? 'X' : 'O';
-        isPlayerOneTurn = !isPlayerOneTurn;
-        winner = _checkForWinner();
-      });
-    }
-  }
-
-  String _checkForWinner() {
-    // Check rows
-    for (int i = 0; i < 3; i++) {
-      if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0].isNotEmpty) {
-        return board[i][0];
-      }
-    }
-
-    // Check columns
-    for (int i = 0; i < 3; i++) {
-      if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i].isNotEmpty) {
-        return board[0][i];
-      }
-    }
-
-    // Check diagonals
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0].isNotEmpty) {
-      return board[0][0];
-    }
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2].isNotEmpty) {
-      return board[0][2];
-    }
-
-    // Check for a tie
-    if (!board.any((row) => row.any((cell) => cell.isEmpty))) {
-      return 'Tie';
-    }
-
-    return '';
-  }
-
-  void _resetGame() {
-    setState(() {
-      board = List.generate(3, (i) => List.filled(3, ""));
-      isPlayerOneTurn = true;
-      winner = "";
-    });
   }
 }
